4803050d5c229ed7e44a710d68d5fef6808ca780
Dmitry Zhukov <killboris21@gmail.com>
2024-11-13 21:58:25 +0300

    Changed logics. Add class Box

--- src/main/java/com/stepup/proj/Main.java
+++ src/main/java/com/stepup/proj/Main.java
@@ -3,7 +3,10 @@ package com.stepup.proj;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world!2");
+        System.out.println("664642");
+
         System.out.println("Hello 33333!2");
+        System.out.println("Hello 5555!2");
+        System.out.println("Hello 323!2");
     }
 }
